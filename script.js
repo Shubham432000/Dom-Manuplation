22090e9dfa339de36aeb4f2e67e8014ad1ad1fee
Shubham432000 <shubham7410112997@gmail.com>
2022-12-07 21:46:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+1.
+
+document.querySelector(
+  ".crayons-layout .sidebar-wrapper .side-bar .crayons-card .crayons-subtitle-2"
+).innerHTML = "SHUBHAM";
+
+document.querySelector(
+  ".crayons-layout .sidebar-wrapper .side-bar .crayons-card  .color-base-70 "
+).innerHTML = "I WRITE CODE";
+
+2.
+
+let navbar = document.querySelector(".accordion-homepage");
+let section = document.createElement("section");
+section.classList = "parent";
+let h3 = document.createElement("h3");
+h3.innerHTML = "my new faq";
+section.appendChild(h3);
+navbar.appendChild(section);
+
+3.
+
+document.querySelector(".customer-support .one-tel-number").innerHTML =
+  "+91 6366256689";
+
+4.
+
+document.querySelector(
+  ".mytabs .diwali-deals-product-sale-pro .diwali-deals-product-sale-btn"
+).innerHTML = "Check Out";
+
+5.
+
+let button = document.querySelector(".searchinput___19uW0");
+
+button.style.backgroundColor = "red";
+
+
+6.
+
+document.querySelector(
+  ".content-width-extra-large .display-heading-1"
+).style.fontFamily = "monospace";
+
+document.querySelector(
+  ".content-width-extra-large .display-heading-1"
+).style.color = "#A7331C";
+
+7.
+let btns = document.querySelector(".js-repos-container .f4 .btn")
+btns.style.backgroundColor = "blue";
+
+8.
+document.querySelector(".fl-heading .fl-heading-text").innerHTML ="JS bootcamp"
+
+9.
+document.querySelector(".HotDealsAll__Heading__2fIbe").style.fontSize = "80px"
+
+10.
+let logo = document.querySelector(" .header .wrapper .logo .icon")
+logo.style.backgroundImage = "url('https://ineuron.ai/images/ineuron-logo.png')"; 
